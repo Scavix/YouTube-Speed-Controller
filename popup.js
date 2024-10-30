484e10b8a8a1e054a092e4217f74fbaf112aaa7a
PerMyFiore <42140533+Scavix@users.noreply.github.com>
2024-10-30 22:57:44 +0100

    update

--- popup.js
+++ popup.js
@@ -91,6 +91,21 @@ function speed3() {
   const videoPlayer = document.querySelector('video');
   if (videoPlayer) {
     videoPlayer.playbackRate = 3;
-    document.getElementById('speed').innerHTML = '3';
+  }
+}
+
+document.getElementById('speed10').addEventListener('click', async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: speed10
+  });
+});
+
+function speed10() {
+  const videoPlayer = document.querySelector('video');
+  if (videoPlayer) {
+    videoPlayer.playbackRate = 10;
   }
 }
